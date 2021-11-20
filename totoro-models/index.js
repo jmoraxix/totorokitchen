becba99f0b202081db0f6011578de988b6a01beb
Jadrimva92 <74443563+Jadrimva92@users.noreply.github.com>
2021-11-20 01:57:28 -0600

    Index Models
    Add module exports

--- totoro-models/index.js
+++ totoro-models/index.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose');
 
+const ClaseComestible = require('./models/ClaseComestible');
+const DetalleUnidad = require('./models/DetalleUnidad');
+const Empresa = require('./models/Empresa');
+const EstadoOrden = require('./models/EstadoOrden');
+const Evento = require('./models/Evento');
+const LineaComestible = require('./models/LineaComestible');
 const Pais = require('./models/Pais');
 const Persona = require('./models/Persona');
+const Rol = require('./models/Rol');
+const TipoBebida = require('./models/TipoBebida');
+const TipoComestible = require('./models/TipoComestible');
+const TipoComida = require('./models/TipoComida');
+const TipoProducto = require('./models/TipoProducto');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb+srv://Admin:QRwAb1x0VruYHgwL@cltadriano.req5g.mongodb.net/restaurante?retryWrites=true&w=majority', {
@@ -10,6 +21,17 @@ mongoose.connect('mongodb+srv://Admin:QRwAb1x0VruYHgwL@cltadriano.req5g.mongodb.
 })
 
 module.exports = {
+    ClaseComestible: ClaseComestible,
+    DetalleUnidad: DetalleUnidad,
+    Empresa: Empresa,
+    EstadoOrden: EstadoOrden,
+    Evento: Evento,
+    LineaComestible: LineaComestible,
     Pais: Pais,
-    Persona: Persona
+    Persona: Persona,
+    Rol: Rol,
+    TipoBebida: TipoBebida,
+    TipoComestible: TipoComestible,
+    TipoComida: TipoComida,
+    TipoProducto: TipoProducto
 };
