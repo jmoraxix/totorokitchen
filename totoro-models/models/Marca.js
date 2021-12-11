e06c0efd380cca31fed3756dbbbd43262418a253
Jadrimva92 <74443563+Jadrimva92@users.noreply.github.com>
2021-12-10 23:29:50 -0600

    Create Marca.js

---
+++ totoro-models/models/Marca.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Empresa = require('./Empresa');
+const Pais = require('./Pais');
+
+
+const Schema = mongoose.Schema;
+
+const marcaSchema = new Schema({
+    codigo: {
+        type: String,
+        trim: true,
+        unique: true,
+        uppercase: true
+    },
+    nombre: {
+        type: String,
+        trim: true
+    },
+    descripcion: {
+        type: String,
+        trim: true
+    },
+    empresa: {
+        type: Empresa,
+        default: {}
+    },
+    pais: {
+        type: Pais,
+        default: {}
+    }
+})
+
+module.exports = mongoose.model('Marca', marcaSchema);
